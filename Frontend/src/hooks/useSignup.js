528070bf6965023441d4d766f997e5e20db69504
Kaan <sntrkkaan@gmail.com>
2024-05-03 18:33:35 +0300

    bug fixed

--- Frontend/src/hooks/useSignup.js
+++ Frontend/src/hooks/useSignup.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
 
-  const signup = async(fullName,username,password,confirmPassword,gender) => {
+  const signup = async({fullName,username,password,confirmPassword,gender}) => {
     const success = handleInputErrors({fullName,username,password,confirmPassword,gender})
     if(!success) return;
 
