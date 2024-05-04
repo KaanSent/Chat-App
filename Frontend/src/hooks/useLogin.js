00808934f2d34347ae3d5562268292d8352ffa0b
Kaan <sntrkkaan@gmail.com>
2024-05-04 14:41:33 +0300

    hot fix login func

--- Frontend/src/hooks/useLogin.js
+++ Frontend/src/hooks/useLogin.js
@@ -3,35 +3,46 @@ import { useAuthContext } from "../context/AuthContext"
 import { useState } from "react"
 
 const useLogin = () => {
-  const [loading, setLoading] = useState(false)
-  const {setAuthUser} = useAuthContext();
+    const [loading, setLoading] = useState(false)
+    const {setAuthUser} = useAuthContext();
 
-  const login = async ({username, password}) => {
-    setLoading(true)
-    try {
-      const res = await fetch('/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({username, password})
-      })
-      const data = await res.json()
-      if(data.error){
-        throw new Error(data.error)
-      }
+    const login = async ({username, password}) => {
+        const success = handleInputErrors(username, password)
+        if(!success) return;
 
-      localStorage.setItem('chat-user', JSON.stringify(data))
-      setAuthUser(data)
+        setLoading(true)
+        try {
+            const res = await fetch('/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, password})
+            })
+            const data = await res.json()
+            if(data.error){
+                throw new Error(data.error)
+            }
 
-    } catch (error) {
-      toast.error(error.message)
-    }finally{
-      setLoading(false)
+            localStorage.setItem('chat-user', JSON.stringify(data))
+            setAuthUser(data)
+
+        } catch (error) {
+            toast.error(error.message)
+        }finally{
+            setLoading(false)
+        }
     }
-    
-  }
-  return {loading, login}
+
+    return {loading, login}
 }
 
-export default useLogin
+function handleInputErrors(username, password){
+    if( !username || !password ){
+        toast.error("please fill all fields")
+        return false
+    }
+    return true
+}
+
+export default useLogin;
